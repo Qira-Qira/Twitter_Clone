ba977f6cefe8176b9946d64e8c7572181cc513fc
Qira-Qira <nawafariq3@gmail.com>
2025-05-23 23:02:13 +0700

    1- update user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -95,6 +95,14 @@ export const updateUser = async (req, res) => {
 
     const userId = req.user._id;
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                error: "Invalid email address"
+            });
+        }
+
     try {
         let user = await User.findById(userId);
         if (!user) return res.status(404).json({ message: "User not found" });
