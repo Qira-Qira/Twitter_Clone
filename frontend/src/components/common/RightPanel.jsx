366d0a7439de98e1ac03d11278cbf5ff300b004f
Qira-Qira <nawafariq3@gmail.com>
2025-06-02 19:21:22 +0700

    3. fix bug suggested users fetched

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -27,7 +27,12 @@ const RightPanel = () => {
 
   const { follow, isPending } = useFollow();
 
-  if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
+  if (
+    Array.isArray(suggestedUsers?.suggestedUsers) &&
+    suggestedUsers.suggestedUsers.length === 0
+  ) {
+    return <div className="md:w-64 w-0"></div>;
+  }
 
   return (
     <div className="hidden lg:block my-4 mx-2">
