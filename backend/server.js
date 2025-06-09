6da8672e0c56c5b09d2298db2b2cd378acf209c3
Qira-Qira <nawafariq3@gmail.com>
2025-06-10 00:14:11 +0700

    4. deployment

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
+import path from "path";
+
 dotenv.config();
 
 cloudinary.config({
@@ -21,17 +23,24 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cookieParser());
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: '5mb' })); // to parse req.body
-// limit should'nt be too high to prevent DOS
+app.use(cookieParser());
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit should"nt be too high to prevent DOS
 app.use(express.urlencoded({ extended: true }));// to parse urlencoded data
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/notifications', notificationRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     connectMongoDB();
